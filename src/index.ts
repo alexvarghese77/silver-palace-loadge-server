1ca6340d79106c868a255d37be2aad8654da39ad
xela <alex.varghese77@gmail.com>
2019-09-18 01:53:14 -0400

    initialcommit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {SilverPalaceLoadgeServerApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {SilverPalaceLoadgeServerApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new SilverPalaceLoadgeServerApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
